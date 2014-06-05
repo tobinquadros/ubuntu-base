213a0a82edff683eb9d9ec4751d2aa001c3ac342
Tobin Quadros <tobinquadros@gmail.com>
2014-06-04 22:03:34 -0500

    Remove email from git global config. Update ignores.
    Also started to remove need for sudo in avrdude, didn't work.

--- embedded/make_avr.sh
+++ embedded/make_avr.sh
@@ -7,3 +7,9 @@ sudo apt-get install -y avrdude avrdude-doc
 sudo apt-get install -y avr-libc
 sudo apt-get install -y binutils-avr
 
+## avrdude must be run as root unless udev has a rule added,
+## this added a rule file but didn't seem to work. Suggestions?
+# sudo touch /etc/udev/rules.d/62-usbtiny.rules
+# sudo chown root:root /etc/udev/rules.d/62-usbtiny.rules
+# sudo chmod 644 /etc/udev/rules.d/62-usbtiny.rules
+# echo 'echo SUBSYSTEM=="usb",\ ATTR{product}=="USBtiny",\ ATTR{idProduct}=="0c9f",\ ATTRS{idVendor}=="1781",\ MODE="0660",\ GROUP="dialout" > /etc/udev/rules.d/62-usbtiny.rules' | sudo -s
