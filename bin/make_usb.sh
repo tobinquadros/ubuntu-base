7c2e0d776b2d722ab08314a00601952bb930ff08
Tobin Quadros <tobin.quadros@lonelyplanet.com>
2018-02-14 14:42:10 -0600

    minor tweaks to usb script

--- bin/make_usb.sh
+++ bin/make_usb.sh
@@ -6,26 +6,38 @@ echo "Source File: " && read SOURCE
 
 # Choose a drive to put the installer on.
 echo -n "Which disk would you like to put the installer on? (eg. disk2): " && read DISK
+
 if [[ $DISK =~ disk[0-9] ]]; then
+
   # Confirm disk selection before overwriting
   read -p "Erase /dev/$DISK and all it's content to create installer? (y/n) " -n 1; echo ""
+
   if [[ $REPLY =~ ^[Yy]$ ]]; then
+
     # Prepare device to accept installer
-    diskutil partitionDisk /dev/$DISK GPT free Ubuntu R 
+    diskutil partitionDisk /dev/$DISK GPT free ubuntu R
     diskutil unmountDisk /dev/$DISK
+
     # Copy installer to USB disk
-    echo "Ready to copy(dd) $SOURCE to /dev/$DISK."
+    echo "Ready to copy (dd) $SOURCE to /dev/$DISK."
     sudo dd if=$SOURCE of=/dev/r${DISK} bs=1m
+
     # Print success if dd finished with no error
-    [[ $? -eq 0 ]] && echo "Install complete, reboot your Mac while holding down option."
+    [[ $? -eq 0 ]] && echo "Write completed successfully."
+
   else
+
     # User did not reply with a 'Y' or 'y' confirmation
     echo "Ok, exiting without creating installer."
     exit
+
   fi
+
 else
+
   # User disk selection didn't pass regex validation
-  echo "You didn't select a proper disk. Exiting..."
-  exit
+  echo "ERROR: You didn't select a proper disk. Exiting..."
+  exit 1
+
 fi
 
